10452f8895136b49fff3338abde0267ce405bed3
Yeboah Peter <peteryeboah09@gmail.com>
2024-06-11 15:48:01 +0100

    Slide complete

--- app/index.js
+++ app/index.js
@@ -1,7 +1,51 @@
 const hamBtn = document.querySelector('.js-btn-cont');
-const ulHam = document.querySelector('.js-ham-ul')
+const ulHam = document.querySelector('.js-ham-ul');
 
 hamBtn.addEventListener('click', ()=> {
   hamBtn.style.background = 'green';
   console.log(ulHam.classList.add('show'));
-})
+});
+
+
+const slider = document.querySelector('.slider');
+
+
+
+let slideIndex = 0;
+
+initSlide()
+function initSlide() {
+  let i;
+  let slides = document.querySelectorAll('.slides');
+ 
+  let index; 
+  
+  for (i = 0; i < slides.length; i++) {
+
+    index =slides[i]
+  
+
+    index.style.display = 'none';
+  
+    
+  }
+  slideIndex ++;
+
+  if (slideIndex >= slides.length) {
+    slideIndex = 1;
+    index.style.left = '-20%';
+  };
+
+ 
+
+slides[slideIndex-1].style.display = 'block';
+
+
+ setTimeout(initSlide, 6000);
+
+};
+
+
+
+
+
