b09132186c610c9606afef10c11b8d65015d0e87
Yeboah Peter <peteryeboah09@gmail.com>
2024-06-12 23:19:17 +0100

    medium screen slide

--- app/index.js
+++ app/index.js
@@ -26,21 +26,29 @@ let currentScroll = 0;
 let intervalId;
 const scrollRight = document.querySelector('.slider');
 
-intervalId = setInterval(() => {
-  currentScroll += 350;
-  scrollRight.scrollTo(currentScroll, 0);
-  if (currentScroll >= slider.scrollWidth) {
-   
-    currentScroll = -350;
-  }
-},3000)
+const mediaQuery1 = window.matchMedia("(max-width:  47.93em)")
+if (mediaQuery1.matches) {
+  intervalId = setInterval(() => {
+    currentScroll += 350;
+    scrollRight.scrollTo(currentScroll, 0);
+    if (currentScroll >= slider.scrollWidth) {
+     
+      currentScroll = -350;
+    }
+  },3000)
+  
+} else {
+  ''
+}
 
+let slideIndex = 0;
+const mediaQuery = window.matchMedia("(min-width: 47.94em)");
+currentScroll = 0;
 
-
-/* let slideIndex = 0;
-
-initSlide()
+if (mediaQuery.matches) {
+  initSlide()
 function initSlide() {
+
   let i;
   let slides = document.querySelectorAll('.slides');
  
@@ -48,7 +56,7 @@ function initSlide() {
   
   for (i = 0; i < slides.length; i++) {
 
-    index =slides[i]
+    index = slides[i];
   
 
     index.style.display = 'none';
@@ -59,7 +67,6 @@ function initSlide() {
 
   if (slideIndex >= slides.length) {
     slideIndex = 1;
-    index.style.left = '-20%';
   };
 
  
@@ -71,6 +78,4 @@ slides[slideIndex-1].style.display = 'block';
 
 };
 
-
-
- */
+}
